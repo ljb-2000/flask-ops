59c0cd4b0d3e97a56a456bf952aae8a339d64051
yangxiangyu <yangxiangyu@live.cn>
2014-12-19 00:49:52 +0800

    echart test

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from app import app
+from flask import render_template
+import json
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template("index.html.bak")
+@app.route('/data')
+def data():
+    obj={'name':"aa",'value':'11'}
+    return json.dumps(obj)
